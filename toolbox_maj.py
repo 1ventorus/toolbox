6d5628a05171230bf711d1ae398835311fd5b5f4
1ventorus <113920033+1ventorus@users.noreply.github.com>
2023-11-26 22:16:36 +0100

    Update toolbox_maj.py

--- toolbox_maj.py
+++ toolbox_maj.py
@@ -13,10 +13,9 @@ def fetch_file(url, filename):
 def install_toolbox():
     system = platform.system()
     if system == "Windows":
-        fichier ="toolbox_win.py"
 
-        if os.path.exists(fichier):
-            os.remove(fichier)
+        if os.path.exists("toolbox_win.py"):
+            os.remove("toolbox_win.py")
             time.sleep(0.5)
             fetch_file("https://raw.githubusercontent.com/1ventorus/toolbox/main/toolbox_win.py", "toolbox_win.py")
             print("toolbox_win.py a été récupéré depuis GitHub.")
@@ -25,10 +24,9 @@ def install_toolbox():
             print("Le fichier n'existe pas.")
         
     elif system == "Linux":
-        fichier ="toolbox_linux.py"
 
-        if os.path.exists(fichier):
-            os.remove(fichier)
+        if os.path.exists("toolbox_linux.py"):
+            os.remove("toolbox_linux.py")
             time.sleep(0.5)
             fetch_file("https://raw.githubusercontent.com/1ventorus/toolbox/main/toolbox_linux.py", "toolbox_linux.py")
             print("toolbox_linux.py a été récupéré depuis GitHub.")
@@ -40,7 +38,8 @@ def install_toolbox():
         print("Système non pris en charge. recommencez lorsque votre systeme sera pris en charge")
         return
 
-    if os.path.exists("toolbox_setup.py"):
-        os.remove("toolbox_setup.py")
-        fetch_file("https://raw.githubusercontent.com/1ventorus/toolbox/main/toolbox_setup.py", "toolbox_setup.py")
+    os.remove("toolbox_setup.py")
+    fetch_file("https://raw.githubusercontent.com/1ventorus/toolbox/main/toolbox_setup.py", "toolbox_setup.py")
 
+if __name__ =="__main__":
+    install_toolbox()
