9dee6aa3ddabcbf0dca7a43650bfbb65cb211376
1ventorus <113920033+1ventorus@users.noreply.github.com>
2023-11-27 21:33:20 +0100

    Create toolbox_config.py

---
+++ toolbox_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from colorama import*
+
+import os
+import json
+
+config = {
+    "couleur":Fore.MAGENTA
+    "command_colors":Fore.RED
+    "entry":">>>"
+}
+
+with open("toolbox_config.json", "w+") as json_file:
+    json.dump(config, json_file)
